2ff14c37188f5c366f8463e56679c555a012820f
lilianagzz <91097320+lilianagzz@users.noreply.github.com>
2026-02-04 21:16:21 -0600

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -16,7 +16,7 @@ def recvline(conn):
 
 
 def listar():
-    os.system("ps -eo pid,comm | head -n 100")
+    os.system("ps -eo pid,comm | tail -n 30")
 
 def iniciar(conn):
     cmd = recvline(conn)              
@@ -63,6 +63,7 @@ while True:
 
 
     conn.close()
+
 
 
 
